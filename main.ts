6b83f66e527ba606f1beca4cc2b9b29e63d058e8
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2020-12-10 12:42:35 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ while (true) {
     console.log("Light Level:" + input.lightLevel())
     if (input.lightLevel() < 5) {
         light.setAll(light.rgb(0, 0, 0))
+    } else if (input.lightLevel()) {
+        light.setAll(light.rgb(0, 0, 255))
     } else {
         light.setAll(light.rgb(100, 100, 100))
     }
