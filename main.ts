1c9f50c6e4fa07f932b624dd22283b31f8b0fa5d
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2020-12-17 18:20:40 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ while (true) {
     } else if (input.lightLevel() > 13) {
         light.setAll(light.rgb(0, 0, 0))
     } else {
-        light.setAll(light.rgb(255, 0, 0))
+        light.setAll(light.rgb(245, 149, 59))
     }
     
 }
